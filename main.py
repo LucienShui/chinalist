95b8e775798aebf1dd590b6cfd1aca111fbe07db
Lucien Shui <lucien@lucien.ink>
2021-01-02 01:58:08 +0800

    Add custom domain support, add modify time comments

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import requests
+import typing
+from datetime import datetime
 
 china_list_url: str = 'https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt'
 
 
+def get_lucien_list() -> typing.List[str]:
+    with open('lucien.txt') as lucien_file:
+        lucien_list: typing.List[str] = lucien_file.read().split()
+
+    return lucien_list
+
+
 def main():
     response: requests.Response = requests.api.get(china_list_url)
-    china_list: str = response.content.decode('utf-8')
+    china_list: typing.List[str] = response.content.decode('utf-8').split()
+    lucien_list: typing.List[str] = get_lucien_list()
 
-    omega_china_list: str = '\n'.join(map(lambda each: f'||{each}', china_list.split()))
+    omega_china_list: typing.List[str] = [*map(lambda each: f'||{each}', lucien_list + china_list)]
+    prefix: typing.List[str] = [
+        f'! Last Modified: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+    ]
+
+    content = '\n'.join(prefix + omega_china_list)
 
     with open('chinalist.txt', 'w') as file:
-        file.write(omega_china_list)
+        file.write(content)
 
 
 if __name__ == '__main__':
